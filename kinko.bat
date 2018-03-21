e90504bcc6aa3082de723b1f50dc35c08abf91e3
Tressley Cahill <tressley@gmail.com>
2018-03-21 11:51:43 -0400

    Reduce file size
    Reduced file size and remove unused lines.

--- kinko.bat
+++ kinko.bat
@@ -1,15 +1,15 @@
 cls
 @ECHO OFF
 title Kinko
-:::   _    _       _         
-:::  | | _(_)_ __ | | _____  
-:::  | |/ / | '_ \| |/ / _ \ 
-:::  |   <| | | | |   < (_) |
-:::  |_|\_\_|_| |_|_|\_\___/ 
-:::                                                     
+:::  _    _       _
+::: | | _(_)_ __ | | _____
+::: | |/ / | '_ \| |/ / _ \
+::: |   <| | | | |   < (_) |
+::: |_|\_\_|_| |_|_|\_\___/
+::: 
 for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 if EXIST "Kinko.{21EC2020-3AEA-1069-A2DD-08002B30309D}" goto UNLOCK
-if NOT EXIST "Unlocked Vault" goto MAKEVAULT
+if NOT EXIST "Unlocked Vault" goto MKVAULT
 :CONFIRM
 echo Are you sure you want to lock the vault? (Y/N)
 set/p "cho=>"
@@ -17,7 +17,7 @@ if %cho%==Y goto LOCK
 if %cho%==y goto LOCK
 if %cho%==N goto END
 if %cho%==n goto END
-echo Invalid choice.
+echo Invalid selection.
 goto CONFIRM
 :LOCK
 ren "Unlocked Vault" "Kinko.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
@@ -26,16 +26,13 @@ echo Vault locked.
 goto END
 :UNLOCK
 echo Enter password:
-set/p "vkey=>"
-if NOT %vkey%==YOUR_KEY_HERE goto END
+set/p "kagi=>"
+if NOT %kagi%==YOUR_KEY_HERE goto END
 attrib -h -s "Kinko.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 ren "Kinko.{21EC2020-3AEA-1069-A2DD-08002B30309D}" "Unlocked Vault"
 echo Vault unlocked successfully.
 goto END
-:FAIL
-echo Invalid password.
-goto END
-:MAKEVAULT
+:MKVAULT
 md "Unlocked Vault"
 echo New vault created successfully.
 goto END
